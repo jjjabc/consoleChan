46b4fb83b528deb689826fe6739a9a2fd05bb938
xunj <xunj@hcsci.com>
2017-06-02 08:19:58 +0800

    change serial lib to github.com/goburrow/serial

--- serial.go
+++ serial.go
@@ -1,12 +1,12 @@
 package consoleChan
 
 import (
-	"github.com/tarm/goserial"
+	"github.com/goburrow/serial"
 )
 
-func SerialOpen(comName string, baudrate int, username, password string) (*Session, error) {
-	conf := &serial.Config{Name: comName, Baud: baudrate}
-	s, err := serial.OpenPort(conf)
+func SerialOpen(comName string, baudrate, dataBits, stopBits int, parity string, username, password string) (*Session, error) {
+	conf := &serial.Config{Address: comName, BaudRate: baudrate, DataBits: dataBits, StopBits: stopBits, Parity: parity}
+	s, err := serial.Open(conf)
 	if err != nil {
 		return nil, err
 	}
